5bd52459dce865fbcfbec2b656b2a5be0450f408
skyswordLi <514226621@qq.com>
2016-04-26 22:32:11 +0800

    Modify codes
    Modify some codes of PCI

--- chapter8/numpredict.py
+++ chapter8/numpredict.py
@@ -1,91 +1,95 @@
 from random import random,randint
 import math
 
+
 def wineprice(rating,age):
-  peak_age=rating-50
-  
-  # Calculate price based on rating
-  price=rating/2
-  if age>peak_age:
-    # Past its peak, goes bad in 10 years
-    price=price*(5-(age-peak_age)/2)
-  else:
-    # Increases to 5x original value as it
-    # approaches its peak
-    price=price*(5*((age+1)/peak_age))
-  if price<0: price=0
-  return price
+    peak_age = rating - 50
+
+    # Calculate price based on rating
+    price = rating/2
+    if age > peak_age:
+        # Past its peak, goes bad in 10 years
+        price = price * (5 - (age - peak_age) / 2)
+    else:
+        # Increases to 5x original value as it
+        # approaches its peak
+        price = price * (5 * ((age + 1) / peak_age))
+    if price < 0:
+        price = 0
+    return price
 
 
 def wineset1():
-  rows=[]
-  for i in range(300):
-    # Create a random age and rating
-    rating=random()*50+50
-    age=random()*50
+    rows = []
+    for i in range(300):
+        # Create a random age and rating
+        rating = random() * 50 + 50
+        age = random() * 50
 
-    # Get reference price
-    price=wineprice(rating,age)
-    
-    # Add some noise
-    price*=(random()*0.2+0.9)
+        # Get reference price
+        price = wineprice(rating, age)
 
-    # Add to the dataset
-    rows.append({'input':(rating,age),
-                 'result':price})
-  return rows
+        # Add some noise
+        price *= (random() * 0.2 + 0.9)
+
+        # Add to the dataset
+        rows.append({'input': (rating, age), 'result': price})
+    return rows
+
 
 def euclidean(v1,v2):
-  d=0.0
-  for i in range(len(v1)):
-    d+=(v1[i]-v2[i])**2
-  return math.sqrt(d)
-
+    return math.sqrt(sum([pow((v1[i] - v2[i]), 2) for i in range(len(v1))]))
+    """
+    d = 0.0
+    for i in range(len(v1)):
+        d += (v1[i] - v2[i]) ** 2
+    return math.sqrt(d)
+    """
 
 def getdistances(data,vec1):
-  distancelist=[]
-  
-  # Loop over every item in the dataset
-  for i in range(len(data)):
-    vec2=data[i]['input']
-    
-    # Add the distance and the index
-    distancelist.append((euclidean(vec1,vec2),i))
-  
-  # Sort by distance
-  distancelist.sort()
-  return distancelist
+    distancelist = []
+
+    # Loop over every item in the dataset
+    for i in range(len(data)):
+        vec2 = data[i]['input']
+
+        # Add the distance and the index
+        distancelist.append((euclidean(vec1, vec2), i))
+
+    # Sort by distance
+    distancelist.sort()
+    return distancelist
 
 def knnestimate(data,vec1,k=5):
-  # Get sorted distances
-  dlist=getdistances(data,vec1)
-  avg=0.0
-  
-  # Take the average of the top k results
-  for i in range(k):
-    idx=dlist[i][1]
-    avg+=data[idx]['result']
-  avg=avg/k
-  return avg
+    # Get sorted distances
+    dlist = getdistances(data, vec1)
+    avg = 0.0
+
+    # Take the average of the top k results
+    for i in range(k):
+        idx = dlist[i][1]
+        avg += data[idx]['result']
+    avg = avg / k
+    return avg
 
 def inverseweight(dist,num=1.0,const=0.1):
-  return num/(dist+const)
+    return num / (dist + const)
 
 def subtractweight(dist,const=1.0):
-  if dist>const: 
-    return 0
-  else: 
-    return const-dist
+    if dist > const:
+        return 0
+    else:
+        return const - dist
 
 def gaussian(dist,sigma=5.0):
-  return math.e**(-dist**2/(2*sigma**2))
+    return math.e ** (-dist ** 2 / (2 * sigma ** 2))
 
 def weightedknn(data,vec1,k=5,weightf=gaussian):
   # Get distances
   dlist=getdistances(data,vec1)
   avg=0.0
   totalweight=0.0
-  
+
   # Get weighted average
   for i in range(k):
     dist=dlist[i][0]
@@ -164,19 +168,19 @@ def probguess(data,vec1,low,high,k=5,weightf=gaussian):
   dlist=getdistances(data,vec1)
   nweight=0.0
   tweight=0.0
-  
+
   for i in range(k):
     dist=dlist[i][0]
     idx=dlist[i][1]
     weight=weightf(dist)
     v=data[idx]['result']
-    
+
     # Is this point in the range?
     if v>=low and v<=high:
       nweight+=weight
     tweight+=weight
   if tweight==0: return 0
-  
+
   # The probability is the weights in the range
   # divided by all the weights
   return nweight/tweight
@@ -193,10 +197,10 @@ def cumulativegraph(data,vec1,high,k=5,weightf=gaussian):
 def probabilitygraph(data,vec1,high,k=5,weightf=gaussian,ss=5.0):
   # Make a range for the prices
   t1=arange(0.0,high,0.1)
-  
+
   # Get the probabilities for the entire range
   probs=[probguess(data,vec1,v,v+0.1,k,weightf) for v in t1]
-  
+
   # Smooth them by adding the gaussian of the nearby probabilites
   smoothed=[]
   for i in range(len(probs)):
@@ -207,6 +211,6 @@ def probabilitygraph(data,vec1,high,k=5,weightf=gaussian,ss=5.0):
       sv+=weight*probs[j]
     smoothed.append(sv)
   smoothed=array(smoothed)
-    
+
   plot(t1,smoothed)
   show()
